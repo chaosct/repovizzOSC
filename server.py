a53d6b0c478f2755dfc218cd8e0812d22c30bf07
Carles F. Julià <carles.fernandez@upf.edu>
2016-05-02 16:47:37 +0200

    can now change the host and port from gui

---
+++ server.py
@@ -0,0 +1,64 @@
+from gevent import monkey
+monkey.patch_all(thread=False)
+
+from time import sleep
+from threading import Thread
+import sys
+
+# ===== Server part =====
+
+import OSC
+from flask import Flask
+from flask.ext.socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+from ws_to_osc import connectaddr
+
+
+def send(msg):
+    osc.sendto(msg, connectaddr)
+
+
+@socketio.on('data', namespace='/osc')
+def ws_play(message):
+    if message['data']:
+        msg = OSC.OSCMessage("/repovizz")
+        msg += message['data']
+        try:
+            send(msg)
+        except OSC.OSCClientError:
+            pass
+
+
+@socketio.on('bulk', namespace='/osc')
+def ws_bulk(message):
+    if message['data']:
+        for row in message['data']:
+            msg = OSC.OSCMessage("/repovizz")
+            msg += row
+            try:
+                send(msg)
+                sleep(0.001)
+            except OSC.OSCClientError:
+                pass
+
+
+def addr_getter():
+    global connectaddr
+    while True:
+        connectaddr = q.get()
+
+
+def server(queue):
+    global osc, q
+    q = queue
+    t = Thread(target=addr_getter)
+    t.daemon = True
+    t.start()
+    osc = OSC.OSCClient()
+    try:
+        socketio.run(app, port=5000)
+    except KeyboardInterrupt:
+        pass
--- ws_to_osc.py
+++ ws_to_osc.py
@@ -1,4 +1,5 @@
 #!/bin/env python
+# -*- coding: utf-8 -*-
 
 """
 repoVizz OSC middleware
@@ -6,87 +7,70 @@ repoVizz OSC middleware
 Carles F. Julià <carles.fernandez@upf.edu> 2016
 """
 
-from gevent import monkey
-monkey.patch_all()
-
-from time import sleep
-from threading import Thread
-from multiprocessing import Process, freeze_support
-import sys
-
-# ===== Server part =====
-
-import OSC
-from flask import Flask
-from flask.ext.socketio import SocketIO
-
-app = Flask(__name__)
-socketio = SocketIO(app)
-connectaddr = ('localhost',6448)
-
-
-def send(msg):
-    osc.sendto(msg,connectaddr)
-
-
-@socketio.on('data', namespace='/osc')
-def ws_play(message):
-    if message['data']:
-        msg = OSC.OSCMessage("/repovizz")
-        msg += message['data']
-        try:
-            send(msg)
-        except OSC.OSCClientError:
-            pass
-
-
-@socketio.on('bulk', namespace='/osc')
-def ws_bulk(message):
-    if message['data']:
-        for row in message['data']:
-            msg = OSC.OSCMessage("/repovizz")
-            msg += row
-            try:
-                send(msg)
-                sleep(0.001)
-            except OSC.OSCClientError:
-                pass
-
-
-def server():
-    global osc, q
-    osc = OSC.OSCClient()
-    try:
-        socketio.run(app, port=5000)
-    except KeyboardInterrupt:
-        pass
-
+from multiprocessing import Process, freeze_support, Queue
+connectaddr = ('localhost', 6448)
 
 # ===== GUI part =====
 
 from Tkinter import *
 import ttk
 
+queue = Queue()
+
 
 def gui():
     root = Tk()
     root.title("repoVizz OSC")
 
-    mainframe = ttk.Frame(root, padding="1 1 12 12")
+    def change_addr(*args):
+        try:
+            queue.put((host.get(), int(port.get())))
+        except ValueError:
+            pass
+
+    mainframe = ttk.Frame(root, padding="3 1 12 12")
     mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
     mainframe.columnconfigure(0, weight=1)
     mainframe.rowconfigure(0, weight=1)
 
-    ttk.Label(mainframe, text="Sending OSC messages to {}:{}".format(*connectaddr)).grid(column=1,row=1, sticky=E)
+    ttk.Label(mainframe, text="Sending OSC messages").grid(
+        column=1, row=1, sticky=E)
+    ttk.Button(mainframe, text="Change", command=change_addr).grid(
+        column=1, row=3, sticky=E)
 
-    for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+    configframe = ttk.Frame(mainframe, padding="2 2 0 0")
+    configframe.grid(column=1, row=2, sticky=(N, W, E, S))
+    # configframe.columnconfigure(0, weight=1)
+    # configframe.rowconfigure(0, weight=1)
+
+    ttk.Label(configframe, text="host ").grid(column=1, row=1, sticky=(W, E))
+    ttk.Label(configframe, text="port ").grid(column=1, row=2, sticky=(W, E))
+
+    host = StringVar()
+    port = StringVar()
+
+    host.set(connectaddr[0])
+    port.set(str(connectaddr[1]))
+
+    ttk.Entry(configframe, width=20, textvariable=host).grid(
+        column=2, row=1, sticky=(W, E))
+    ttk.Entry(configframe, width=7, textvariable=port).grid(
+        column=2, row=2, sticky=(W, E))
+
+    for child in mainframe.winfo_children():
+        child.grid_configure(padx=5, pady=5)
 
     root.mainloop()
 
 
+def server(q):
+    from server import server
+    server(q)
+
+
 def main():
     if '--cli' not in sys.argv:
-        t = Process(target=server)
+        t = Process(target=server, args=(queue,))
         t.daemon = True
         t.start()
         gui()
